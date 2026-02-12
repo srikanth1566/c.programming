bc992995f173e07103edbe8cd70cfa000cc7fd7c
srikanth1566 <srikanthbongu8@gmail.com>
2026-02-12 15:56:31 +0530

    Add files via upload

---
+++ series.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() 
+{
+    int size;
+    printf("enter the size of array \n");
+    scanf("%d",&size);
+    int a[size];
+    for(int i=0;i<=(size-1);i++)
+    {
+        printf("enter the %d value:\n",(i+1));
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<=(size-1);i++)
+    {
+        if(a[i]+1==a[i+1])
+        {
+            printf("%d,%d,",a[i],a[i+1]);
+            i++;
+        }
+        else if(a[i]-1==a[i-1])
+        {
+            printf("%d\n",a[i]);
+        }
+    }
+    return 0;
+}
